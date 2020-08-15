e9791da052d05500c3741c6389ba43cbacc669e1
357392280 <357392280@qq.com>
2020-08-15 14:34:50 +0800

    8.15日14:34提交

--- atcrowdfunding07-member-parent/atcrowdfunding17-member-api/src/main/java/com/atguigu/crowd/api/MySQLRemoteService.java
+++ atcrowdfunding07-member-parent/atcrowdfunding17-member-api/src/main/java/com/atguigu/crowd/api/MySQLRemoteService.java
@@ -3,9 +3,7 @@ package com.atguigu.crowd.api;
 import com.atguigu.crowd.entity.po.MemberPO;
 
 
-import com.atguigu.crowd.entity.vo.DetailProjectVO;
-import com.atguigu.crowd.entity.vo.PortalTypeVO;
-import com.atguigu.crowd.entity.vo.ProjectVO;
+import com.atguigu.crowd.entity.vo.*;
 import com.ydgk.ssm.util.ResultEntity;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,4 +30,12 @@ public interface MySQLRemoteService {
 
     @RequestMapping("/get/project/detail/remote/{projectId}")
     ResultEntity<DetailProjectVO>	getDetailProjectVORemote(@PathVariable("projectId") Integer projectId);
+
+    @RequestMapping("/get/order/project/vo/remote")
+    ResultEntity<OrderProjectVO>	getOrderProjectVORemote(@RequestParam("projectId") Integer projectId,
+                                                            @RequestParam("returnId") Integer returnId);
+    @RequestMapping("/get/address/vo/remote")
+    ResultEntity<List<AddressVO>> getAddressVORemote(@RequestParam("memberId")Integer memberId);
+    @RequestMapping("/save/address/remote")
+    ResultEntity<String> saveAddressRemote(@RequestBody AddressVO addressVO);
 }
